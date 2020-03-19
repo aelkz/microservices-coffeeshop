d3443f28ad9b459e5a4deae7f99a5a3a33ed92dc
Raphael Alex Abreu <rabreu@redhat.com>
2020-03-19 18:19:43 -0300

    Initial Release

--- order-service/src/main/java/com/redhat/microservices/coffeeshop/order/resource/OrderResource.java
+++ order-service/src/main/java/com/redhat/microservices/coffeeshop/order/resource/OrderResource.java
@@ -96,7 +96,7 @@ public class OrderResource {
             absolute = true,
             displayName = "Coffee Shop Orders",
             description = "Metrics to show how many orders with POST http method was fired.",
-            tags = {"orders"})
+            tags = {"counter=orders"})
     @Timed(name = "orderProcessingDuration")
     @Metered(name = "orderRequest", tags = {"spec=JAX-RS", "level=REST", "method=POST"})
     public Response order(@Context HttpHeaders headers, PaymentOrder order) throws InterruptedException, ExecutionException {
